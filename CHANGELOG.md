3670760322e43f399e227f3773aee3f895ad4ad8
WMXPY <wm@mengw.io>
2020-02-24 15:50:51 -0600

    deploy 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
 ## 0.0.1
 
-* Init Package
+- Init Package
   
 ## 0.1.0
 
-* Add Controller and Schema Pattern
+- Add Controller and Schema Pattern
+
+## 1.0.0
+
+- Refactor to form validation lib
--- lib/src/fields/checkbox.dart
+++ lib/src/fields/checkbox.dart
@@ -2,30 +2,52 @@ import 'package:flutter/material.dart';
 
 class CheckboxFormField extends FormField<bool> {
   CheckboxFormField({
-    Widget title,
     @required BuildContext context,
+    Widget title,
+    Widget subtitle,
+    Widget secondary,
     FormFieldSetter<bool> onSaved,
     FormFieldValidator<bool> validator,
+    Color activeColor,
+    Color checkColor,
+    bool dense = false,
     bool initialValue = false,
     bool autovalidate = false,
+    bool isThreeLine = false,
   }) : super(
           onSaved: onSaved,
           validator: validator,
           initialValue: initialValue,
           autovalidate: autovalidate,
           builder: (FormFieldState<bool> state) {
-            return CheckboxListTile(
-              dense: state.hasError,
-              title: title,
-              value: state.value,
-              onChanged: state.didChange,
-              subtitle: state.hasError
-                  ? Text(
-                      state.errorText,
-                      style: TextStyle(color: Theme.of(context).errorColor),
-                    )
-                  : null,
-              controlAffinity: ListTileControlAffinity.leading,
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CheckboxListTile(
+                    activeColor: activeColor,
+                    checkColor: checkColor,
+                    dense: dense,
+                    title: title,
+                    subtitle: subtitle,
+                    secondary: secondary,
+                    value: state.value,
+                    onChanged: state.didChange,
+                    isThreeLine: isThreeLine,
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: state.hasError
+                        ? Text(
+                            state.errorText,
+                            style:
+                                TextStyle(color: Theme.of(context).errorColor),
+                          )
+                        : null,
+                  )
+                ],
+              ),
             );
           },
         );
