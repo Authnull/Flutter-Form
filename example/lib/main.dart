26d76af8232b6940f4eb9ceb25ad1c96ed1271cb
WMXPY <wm@mengw.io>
2020-03-10 11:20:16 -0500

    add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Map<String, dynamic> _data;
+  Map<String, dynamic> _data = {
+    "checkbox": false,
+  };
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,21 +22,40 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Flutter Form Example'),
         ),
-        body: Form(
-          child: Column(
-            children: <Widget>[
-              CheckboxFormField(
-                initialValue: false,
-                title: Text("Checkbox Form Field"),
-                context: context,
-                onSaved: (bool value) {
-                  if (value) {
-                    this._data["checkbox"] = value;
-                  }
-                },
-              )
-            ],
-          ),
+        body: Column(
+          children: <Widget>[
+            Form(
+              key: this._formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CheckboxFormField(
+                    initialValue: false,
+                    title: Text("Checkbox Form Field"),
+                    context: context,
+                    onSaved: (bool value) {
+                      this._data["checkbox"] = value;
+                    },
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Text(
+                      "Value: " + (this._data["checkbox"] ? "true" : "false"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            FlatButton(
+              child: Text("Save"),
+              onPressed: () {
+                if (this._formKey.currentState.validate()) {
+                  this._formKey.currentState.save();
+                  setState(() {});
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
