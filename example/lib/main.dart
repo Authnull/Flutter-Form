42dec1a706c890fc73548efa1c08b29a69eb6e97
WMXPY <wm@mengw.io>
2020-03-10 11:11:46 -0500

    remove ios android buildfile

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_form/flutter_form.dart';
 
 void main() => runApp(MyApp());
@@ -12,43 +10,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterForm.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  Map<String, dynamic> _data;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Flutter Form Example'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Form(
+          child: Column(
+            children: <Widget>[
+              CheckboxFormField(
+                initialValue: false,
+                title: Text("Checkbox Form Field"),
+                context: context,
+                onSaved: (bool value) {
+                  if (value) {
+                    this._data["checkbox"] = value;
+                  }
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
